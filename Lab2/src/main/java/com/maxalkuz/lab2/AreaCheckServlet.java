dbbae3a28739a68c2caa6e9f76cb799ad6087d16
Max Kuznetsov <maxalkuz@gmail.com>
2021-11-13 00:36:15 +0300

    Added lab 2

---
+++ Lab2/src/main/java/com/maxalkuz/lab2/AreaCheckServlet.java
@@ -0,0 +1,44 @@
+package com.maxalkuz.lab2;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AreaCheckServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        ArrayList tableRows = (ArrayList) getServletContext().getAttribute("tableRows");
+        if (tableRows == null) {
+            tableRows = new ArrayList<>();
+            getServletContext().setAttribute("tableRows", tableRows);
+        }
+        double x = Double.parseDouble(req.getParameter("x"));
+        double y = Double.parseDouble(req.getParameter("y"));
+        double r = Double.parseDouble(req.getParameter("r"));
+        String key = req.getParameter("key");
+        try (PrintWriter writer = resp.getWriter()) {
+            if (checkData(x, y, r, key)) {
+                tableRows.add(new Point(x, y, r).toHtmlCode());
+                writer.println(tableRows.get(tableRows.size() - 1));
+            } else resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    private boolean checkData(double x, double y, double r, String key) {
+        Double[] xValues = {-3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0};
+        if (key.equals("button")) {
+            return (Arrays.asList(xValues).contains(x) && (y > -5 && y < 5) && (r > 2 && r < 5));
+        }
+        else if (key.equals("svg")){
+            return (r > 2 && r < 5);
+        }
+        else return false;
+    }
+}
